53fc2207ae7547d5e2bde4c3084569fdf9944be7
fouadwed <wedfouad2@gmail.com>
2022-08-30 00:26:04 +0200

    Task1 ITI

---
+++ lib/Profile Task.dart
@@ -0,0 +1,165 @@
+// ignore: depend_on_referenced_packages
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class FirstTaskScreen extends StatelessWidget {
+  const FirstTaskScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar:AppBar(
+          title: const Text("Profile"),
+          centerTitle: true,
+          actions: const [
+            Icon(
+                Icons.favorite
+            ),
+            SizedBox(width: 15,),
+            Icon(
+              Icons.menu,
+            ),
+            SizedBox(width: 15,),
+          ],
+          leading: const Icon(Icons.arrow_back_ios),
+        ),
+
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 10,),
+              const CircleAvatar(
+                maxRadius: 60,
+                backgroundImage: NetworkImage(
+                    'https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-1/295982922_5173674382754865_3189242267481034179_n.jpg?stp=dst-jpg_p240x240&_nc_cat=106&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeHfDmvAgVWImetpIaQcOR-GAvI0NFr5dicC8jQ0Wvl2J2PlWPpzj9f87TpRmtwCkq2asyD-DtS5vI5fu5hDRz5X&_nc_ohc=GZrXmhogPtsAX_ew9l2&_nc_ht=scontent.fcai19-6.fna&oh=00_AT9Vwn6GBbulNt2bqOd_oFjPFx5eh1w3gM-I7OllyvMANA&oe=6311F1B4'
+                ),
+              ),
+              const Text(
+                "WED",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30
+                ),
+              ),
+              const SizedBox(height: 5,),
+              const Text(
+                "MOBILE DEVELOPER",
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20
+                ),
+              ),
+              const SizedBox(height: 10,),
+              const Text(
+                "Creat great projects",
+                style: TextStyle(
+                    fontSize: 20
+                ),
+              ),
+              const Text(
+                "@TwWorks",
+                style:
+                TextStyle(
+                  fontSize: 20,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+              const SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // ignore: sized_box_for_whitespace
+                  Container(
+                      height:50,
+                      width: 50,
+                      child: const Icon(
+                        FontAwesomeIcons.facebook,
+                        color: Color.fromARGB(255, 12, 51, 117),
+                        size: 30,
+                      )
+                  ),
+                  // ignore: sized_box_for_whitespace
+                  Container(
+                    height:50,
+                    width: 50,
+                    child: const Icon(
+                      FontAwesomeIcons.linkedin,
+                      color: Color.fromARGB(255, 12, 51, 117),
+                      size: 30,
+                    ),
+                  ),
+                  // ignore: sized_box_for_whitespace
+                  Container(
+                    height:50,
+                    width: 50,
+                    child: const Icon(
+                      FontAwesomeIcons.github,
+                      color: Color.fromARGB(255, 12, 51,117),
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 50,),
+              Padding(
+                padding: const EdgeInsetsDirectional.only(
+                  start: 40.0,
+                  end: 40.0,
+                ),
+                child: Container(
+                  width: double.infinity,
+                  height: 2.0,
+                  color: Colors.blueAccent[100],
+                ),
+              ),
+              const SizedBox(height: 50,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  // ignore: sized_box_for_whitespace
+                  Container(
+                    height: 100,
+                    width: 150,
+                    child: const ListTile(
+                      title: Icon(
+                        Icons.arrow_circle_right,
+                        size: 50,color: Colors.indigo,
+                      ),
+                      subtitle: Text(
+                        "1.3K \n Followers",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22
+                        ),
+                        textAlign: TextAlign.center,),
+                    ),
+                  ),
+                  // ignore: sized_box_for_whitespace
+                  Container(
+                    height: 100,
+                    width:150,
+                    child: const ListTile(
+                      title: Icon(
+                        Icons.arrow_circle_right,
+                        size: 50,color: Colors.indigo,
+                      ),
+                      subtitle: Text(
+                        "1.3K \n Followers",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
